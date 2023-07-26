e5c597d68d4cb9c632a32a0e264717df54f8cda7
Popular2000 <135134957+Popular2000@users.noreply.github.com>
2023-07-26 14:08:16 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+const App = () => {
+  return (
+    <div className="container">
+    <h1>Calculator Design</h1>
+    <div className="calculator">
+        <div className="top">
+            <h1>0</h1>
+        </div>
+        <div class="bottom">
+            <div id="first">AC</div>
+            <div id="first">/</div>
+            <div id="first">x</div>
+            <div id="second"></div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+            <div id="second">-</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div id="second">+</div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div id="equal">=</div>
+            <div id="point">.</div>
+            <div>0</div>
+            <div>%</div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
